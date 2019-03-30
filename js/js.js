0f89c7ce08038dcf85ab9fa1d959c19c643b4b97
Alex Sebstian <42471288+AlexSebstian@users.noreply.github.com>
2019-03-30 19:38:55 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,8 +1,11 @@
+
+
 function run(){
 
 	var V=document.getElementById("V").value;
 	var s=828;
 	var tot=0;
-        tot=2650*V/s+5000+1325;
-        alert('共需航油（KG）：'+tot);
+	tot=2650*V/s+5000;
+    alert('共需航油（KG）：'+tot);
+
 }
