0437a99a32cd87aac09184228bbd17c4d7c9675b
Alex Sebstian <42471288+AlexSebstian@users.noreply.github.com>
2019-03-30 19:29:48 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,17 +3,6 @@ function run(){
 	var V=document.getElementById("V").value;
 	var s=828;
 	var tot=0;
-	if(V<30)
-        {
-            alert(’距离过短，不予计算！’);
-        }
-        if(tot>20896)
-        {
-           alert(’油量过多！不予计算！’);
-        }
-	else
-	{
 		tot=2650*V/s+5000+1325;
-	}
         alert('共需航油（KG）：'+tot);
 }
